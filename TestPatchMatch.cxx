448f98daf5e9ee9918ee849071085e1547820fa8
Zack Moratto <zachary.m.moratto@nasa.gov>
2013-09-02 12:44:09 -0700

    Calculating RL

--- TestPatchMatch.cxx
+++ TestPatchMatch.cxx
@@ -2,20 +2,21 @@
 #include <vw/Image.h>
 #include <vw/FileIO.h>
 #include <vw/Stereo/DisparityMap.h>
+#include <vw/Stereo/Correlate.h>
 #include <gtest/gtest.h>
 
 #include <boost/random/linear_congruential.hpp>
 #include <boost/random/uniform_01.hpp>
 
 namespace vw {
-template<> struct PixelFormatID<Vector2f> { static const PixelFormatEnum value = VW_PIXEL_GENERIC_2_CHANNEL; };
+  template<> struct PixelFormatID<Vector2f> { static const PixelFormatEnum value = VW_PIXEL_GENERIC_2_CHANNEL; };
 }
 
 using namespace vw;
 
 // Possibly replace with something that keep resampling when out of bounds?
 inline Vector2f
-clip_to_search_range( Vector2f in, BBox2i const& search_range ) {
+clip_to_search_range( Vector2f& in, BBox2f const& search_range ) {
   if ( in.x() < search_range.min().x() ) in.x() = search_range.min().x();
   if ( in.x() > search_range.max().x() ) in.x() = search_range.max().x();
   if ( in.y() < search_range.min().y() ) in.y() = search_range.min().y();
@@ -54,11 +55,61 @@ float calculate_cost( Vector2f const& a_loc, Vector2f const& b_loc,
 }
 
 // Propogates Left, Above, and against opposite disparity
+//
+// Remember a and ab_disparity don't have the same dimensions. A has
+// been expanded so we don't have to do an edge extension. The same
+// applies to b and ba_disparity.
 void evaluate_even_iteration( ImageView<uint8> const& a, ImageView<uint8> const& b,
                               BBox2i const& a_roi, BBox2i const& b_roi,
                               ImageView<Vector2f>& ab_disparity,
                               ImageView<Vector2f>& ba_disparity,
-                              BBox2i const& search_range ) {
+                              BBox2f const& search_range ) {
+
+  BBox2i b_disp_size = bounding_box(ba_disparity);
+
+  // TODO: This could iterate by pixel accessor using ab_disparity
+  for ( size_t j = 0; j < ab_disparity.rows(); j++ ) {
+    for ( size_t i = 0; i < ab_disparity.cols(); i++ ) {
+      float cost_new;
+      Vector2f d_new = ab_disparity(i,j);
+      Vector2f loc(i,j);
+
+      // TODO: This could be cached!
+      float curr_cost =
+        calculate_cost<15,15>( loc, loc+d_new, a, b, a_roi, b_roi );
+
+      // Comparing left
+      if ( i > 0 ) {
+        d_new = ab_disparity(i-i,j);
+        cost_new = calculate_cost<15,15>( loc, loc+d_new, a, b, a_roi, b_roi );
+        if ( cost_new < curr_cost ) {
+          curr_cost = cost_new;
+          ab_disparity(i,j) = d_new;
+        }
+      }
+      // Comparing top
+      if ( j > 0 ) {
+        d_new = ab_disparity(i,j-1);
+        cost_new = calculate_cost<15,15>( loc, loc+d_new, a, b, a_roi, b_roi );
+        if ( cost_new < curr_cost ) {
+          curr_cost = cost_new;
+          ab_disparity(i,j) = d_new;
+        }
+      }
+
+      // Comparing against RL
+      Vector2f d = ab_disparity(i,j);
+      if ( b_disp_size.contains( d + Vector2f(i,j) ) ) {
+        d_new = -ba_disparity(i+d[0],j+d[1]);
+        cost_new = calculate_cost<15,15>( loc, loc+d_new, a, b, a_roi, b_roi );
+        if ( cost_new < curr_cost ) {
+          curr_cost = cost_new;
+          ab_disparity(i,j) = d_new;
+        }
+      }
+
+    }
+  }
 }
 
 // Propogates Right, Below, and against opposite disparity
@@ -82,6 +133,7 @@ TEST( PatchMatch, Basic ) {
   Vector2f iteration_search_size = Vector2f(search_range.size())/4.0;
   vargen_type random_source(gen, boost::random::uniform_01<>());
   Vector2f search_range_size = search_range.size();
+  BBox2f search_range_rl( -search_range.max(), -search_range.min() );
 
   for (size_t j = 0; j < lr_disparity.rows(); j++ ) {
     for (size_t i = 0; i < lr_disparity.cols(); i++ ) {
@@ -110,69 +162,35 @@ TEST( PatchMatch, Basic ) {
   ImageView<uint8> left_expanded( crop(edge_extend(left_image), left_expanded_roi ) ),
     right_expanded( crop(edge_extend(right_image), right_expanded_roi ) );
 
-  // Propogation 0 LR
-  for ( size_t j = 0; j < lr_disparity.rows(); j++ ) {
-    for ( size_t i = 0; i < lr_disparity.cols(); i++ ) {
-      float cost_new;
-      Vector2f d_new = lr_disparity(i,j);
-      Vector2f loc(i,j);
 
-      float curr_cost =
-        calculate_cost<15,15>( loc, loc+d_new, left_expanded, right_expanded,
-                               left_expanded_roi, right_expanded_roi );
+  // // Apply new search
+  // d_new =
+  //   clip_to_search_range(lr_disparity(i,j) + Vector2f(horizontal_search_noise(),vertical_search_noise()),search_range);;
+  // cost_new = calculate_cost<15,15>( loc, loc+d_new, left_expanded, right_expanded,
+  //                                   left_expanded_roi, right_expanded_roi );
+  // if ( cost_new < curr_cost ) {
+  //   curr_cost = cost_new;
+  //   lr_disparity(i,j) = d_new;
+  // }
 
-      // Comparing left
-      if ( i > 0 ) {
-        d_new = lr_disparity(i-i,j);
-        cost_new = calculate_cost<15,15>( loc, loc+d_new, left_expanded, right_expanded,
-                                          left_expanded_roi, right_expanded_roi );
-        if ( cost_new < curr_cost ) {
-          curr_cost = cost_new;
-          lr_disparity(i,j) = d_new;
-        }
-      }
-      // Comparing top
-      if ( j > 0 ) {
-        d_new = lr_disparity(i,j-1);
-        cost_new = calculate_cost<15,15>( loc, loc+d_new, left_expanded, right_expanded,
-                                          left_expanded_roi, right_expanded_roi );
-        if ( cost_new < curr_cost ) {
-          curr_cost = cost_new;
-          lr_disparity(i,j) = d_new;
-        }
-      }
-
-      // Comparing against RL
-      Vector2f d = lr_disparity(i,j);
-      if ( bounding_box(rl_costs).contains( d + Vector2f(i,j) ) ) {
-        d_new = -rl_disparity(i+d[0],j+d[1]);
-        cost_new = calculate_cost<15,15>( loc, loc+d_new, left_expanded, right_expanded,
-                                          left_expanded_roi, right_expanded_roi );
-        if ( cost_new < curr_cost ) {
-          curr_cost = cost_new;
-          lr_disparity(i,j) = d_new;
-        }
-      }
-
-    }
-  }
-
-      // // Apply new search
-      // d_new =
-      //   clip_to_search_range(lr_disparity(i,j) + Vector2f(horizontal_search_noise(),vertical_search_noise()),search_range);;
-      // cost_new = calculate_cost<15,15>( loc, loc+d_new, left_expanded, right_expanded,
-      //                                   left_expanded_roi, right_expanded_roi );
-      // if ( cost_new < curr_cost ) {
-      //   curr_cost = cost_new;
-      //   lr_disparity(i,j) = d_new;
-      // }
-
-  // Propogation 0 RL
+  evaluate_even_iteration( left_expanded, right_expanded,
+                           left_expanded_roi, right_expanded_roi,
+                           lr_disparity, rl_disparity, search_range );
+  evaluate_even_iteration( right_expanded, left_expanded,
+                           right_expanded_roi, left_expanded_roi,
+                           rl_disparity, lr_disparity, search_range_rl );
 
   write_image("lr_1.tif",lr_disparity);
   write_image("rl_1.tif",rl_disparity);
 
   iteration_search_size /= 2;
+
+  // Write out the final trusted disparity
+  ImageView<PixelMask<Vector2f> > final_disparity =
+    pixel_cast<PixelMask<Vector2f> >( lr_disparity );
+  stereo::cross_corr_consistency_check( final_disparity,
+                                        rl_disparity, 1.0, true );
+  write_image("final_disparity.tif", final_disparity );
 }
 
 int main( int argc, char **argv ) {
